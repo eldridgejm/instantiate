a3dfca6548aceacd9eb8f92192f4fed980a14712
Justin Eldridge <eldridgejm@gmail.com>
2020-12-23 09:39:22 -0800

    Made flake multi-platform.

--- flake.nix
+++ flake.nix
@@ -3,15 +3,25 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
 
-  outputs = { self, nixpkgs }: {
-    instantiate = with import nixpkgs { system = "x86_64-darwin"; };
-      python37Packages.buildPythonPackage rec {
-        name = "instantiate";
-        src = ./.;
-        propagatedBuildInputs = with python37Packages; [ jinja2 pyyaml ];
-        nativeBuildInputs = with python37Packages;[ black pytest ipython ];
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+      {
+        instantiate = forAllSystems (system:
+          with import nixpkgs { system = "${system}"; };
+            python37Packages.buildPythonPackage rec {
+              name = "instantiate";
+              src = ./.;
+              propagatedBuildInputs = with python37Packages; [ jinja2 pyyaml ];
+              nativeBuildInputs = with python37Packages;[ black pytest ipython ];
+            }
+          );
+
+        defaultPackage = forAllSystems (system:
+            self.instantiate.${system}
+          );
       };
-    defaultPackage.x86_64-darwin = self.instantiate;
-  };
 
 }

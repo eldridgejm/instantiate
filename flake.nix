59328b52294912b5c077edfb7048ebb2dffdfa2a
Justin Eldridge <eldridgejm@gmail.com>
2020-12-22 18:48:09 -0800

    Added flake.

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Instantiate directories from templates.";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
+
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux = with import nixpkgs { system = "x86_64-linux"; };
+      python37Packages.buildPythonPackage rec {
+        name = "instantiate";
+        src = ./.;
+        propagatedBuildInputs = with python37Packages; [ jinja2 pyyaml ];
+        nativeBuildInputs = with python37Packages;[ black pytest ipython ];
+      };
+  };
+
+}

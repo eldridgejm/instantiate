998c97042bb5c9a17766fe0f37525fa05ea606a1
Justin Eldridge <eldridgejm@gmail.com>
2021-01-09 16:34:13 -0800

    Bumped version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="instantiate",
-    version="0.1.3",
+    version="0.1.4",
     py_modules=["instantiate"],
     install_requires=["jinja2", "pyyaml"],
     entry_points={"console_scripts": ["instantiate = instantiate:cli"]},
